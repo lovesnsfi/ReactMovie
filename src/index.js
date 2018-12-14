bf04454551a9ee625030889140040932bce1363e
二当家的 <lovesnsfi@163.com>
2018-12-14 22:10:32 +0800

    路由更改

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App.js';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {HashRouter,Route,Switch} from 'react-router-dom';
 import Index from "./pages/Index/Index";
 import Detail from "./pages/Detail/Detail";
     
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render((
-    <BrowserRouter>
+    <HashRouter>
         <Switch>
             <App>
                 <Route path="/index" component={Index} ></Route>
@@ -18,7 +18,7 @@ ReactDOM.render((
             </App>
         </Switch>  
        
-    </BrowserRouter>
+    </HashRouter>
 ), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
